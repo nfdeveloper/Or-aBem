1694dd82183883861f8f9cfa18af580e86e5b661
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-15 12:59:03 -0300

    BackEnd: Criando projeto e estruturando pastas iniciais

---
+++ src/backend/orcabemapi/src/main/java/br/com/nfdeveloper/orcabemapi/OrcabemapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nfdeveloper.orcabemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcabemapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcabemapiApplication.class, args);
+	}
+
+}
---
+++ src/backend/orcabemapi/src/test/java/br/com/nfdeveloper/orcabemapi/OrcabemapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nfdeveloper.orcabemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcabemapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
